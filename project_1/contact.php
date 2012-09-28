2a98f1b61a260564387fd81f766bb0623e4c7e5a
Emma Nelson <eknelson17@gmail.com>
2012-09-28 02:13:42 -0400

    trying to stupid-proof the form submission. - take 3

--- project_1/contact.php
+++ project_1/contact.php
@@ -19,28 +19,45 @@
 				<li><a href="about.html"> About </a> </li>
 				<li><a href="projects.html"> Projects </a> </li>
 				<li><a href="http://eknelson17.tumblr.com/"> Blog </a> </li>
-				<li><a href="#"> Contact </a> </li>
+				<li><a href="contact.php"> Contact </a> </li>
 			</ul>
 		</nav>
 
 		<article>
 
 			<?php
+			function spamcheck($field) {
+				//filter_var() sanitizes the e-mail
+				//address using FILTER_SANITIZE_EMAIL
+				$field=filter_var($field, FILTER_SANITIZE_EMAIL);
+
+				//filter_var() validates the e-mail
+				//address using FILTER_VALIDATE_EMAIL
+				if(filter_var($field, FILTER_VALIDATE_EMAIL)) {
+					return TRUE;
+				} else {
+					return FALSE;
+				}
+			}
+
 			//if "email" is filled out, send email
-			if (isset($_REQUEST['from_email']) and
-				isset($_REQUEST['from_name']) and
-				isset($_REQUEST['subject']) and
-				isset($_REQUEST['message'])) {
-			  	//send email
-				$email = $_REQUEST['from_email'] ;
-				$name = $_REQUEST['from_name'] ;
-				$subject = $_REQUEST['subject'] ;
-				$message = wordwrap($_REQUEST['message'], 70); ;
-				mail('eknelson17@gmail.com', 
-				  	$subject,
-				  	$message, 
-				  	'From: ' . $name . ' <' . $email . '>');
-				echo "Thank you!";
+			if (isset($_REQUEST['from_email'])) {
+				//check if the email address is invalid
+			  	$mailcheck = spamcheck($_REQUEST['email']);
+			  	if ($mailcheck==FALSE) {
+			    	echo "Invalid input";
+			    } else {
+				  	//send email
+					$email = $_REQUEST['from_email'] ;
+					$name = $_REQUEST['from_name'] ;
+					$subject = $_REQUEST['subject'] ;
+					$message = wordwrap($_REQUEST['message'], 70); ;
+					mail('eknelson17@gmail.com', 
+					  	$subject,
+					  	$message, 
+					  	'From: ' . $name . ' <' . $email . '>');
+					echo "Thank you!";
+				}
 			//if "email" is not filled out, display the form
 			} else { 
 				echo "<form action='contact.php' method='POST' enctype='multipart/form-data'>
