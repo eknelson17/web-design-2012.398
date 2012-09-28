3e5ec862c6bb804161df8cfb39309df596e7692e
Emma Nelson <eknelson17@gmail.com>
2012-09-28 01:36:58 -0400

    *think* I fixed the form problem...silly syntax errors.

--- project_1/contact.php
+++ project_1/contact.php
@@ -80,30 +80,30 @@
 			  <input type='submit' />
 			  </form>";
 			  */
-			  echo "<form action="index.html" method="POST" enctype="multipart/form-data">
+			  echo "<form action='index.html' method='POST' enctype='multipart/form-data'>
 						<table>
 							<tr>
-								<td><label for="from_name">Name: </label></td>
-								<td><input type="text" name="from_name" id="from_name" /></td>
+								<td><label for='from_name'>Name: </label></td>
+								<td><input type='text' name='from_name' id='from_name' /></td>
 							</tr>
 
 							<tr>
-								<td><label for="from_email">Email Address: </label></td>
-								<td><input type="text" name="from_email" id="from_email" /></td>
+								<td><label for='from_email'>Email Address: </label></td>
+								<td><input type='text' name='from_email' id='from_email' /></td>
 							</tr>
 
 							<tr>
-								<td><label for="subject">Subject: </label></td>
-								<td><input type="text" name="subject" id="subject" /></td>
+								<td><label for='subject'>Subject: </label></td>
+								<td><input type='text' name='subject' id='subject' /></td>
 							</tr>
 
 							<tr>
-								<td><label for="message">Message: </label></td>
-								<td><textarea type="text" name="message" id="message" rows="3" cols="30"></textarea></td>
+								<td><label for='message'>Message: </label></td>
+								<td><textarea type='text' name='message' id='message' rows='3' cols='30'></textarea></td>
 							</tr>
 
 							<tr>
-								<td><input type="submit" value="Submit" /></td>
+								<td><input type='submit' value='Submit' /></td>
 							</tr>
 						</table>
 					</form>";
